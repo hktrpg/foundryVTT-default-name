ed0fd96bf51744fe17d05426c7e6a2526f2fe79c
Zeteticl <zetet@dasd.com>
2021-02-23 22:31:08 +0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-class defaultName {
-
-    static keepAnEyeOut = function () {
-
-
-    }
-
-}
-Hooks.on("renderDialog", (v) => {
-    console.log(v)
-    let data = v.data.content;
-    v.data.content = null;
-})
-
-Hooks.once("ready", defaultName.keepAnEyeOut);
+Hooks.on("renderDialog", (dialog, $element) => {
+    $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
+    $element.find(`input[type=text]`)[0].focus();
+});
