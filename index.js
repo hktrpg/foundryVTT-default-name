2ad4a845f281f95b2f19fda50c6801ee7e8b8423
zetetic <>
2021-02-23 18:23:47 +0800

    8

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ class defaultName {
 }
 Hooks.on("renderDialog", (v) => {
     console.log(v)
+    let data = v.data.content;
+    v.data.content = null;
 })
 
 Hooks.once("ready", defaultName.keepAnEyeOut);
