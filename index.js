d0f36c0c50fbdbf1528a4a007ee90f6a9a2359da
Zeteticl <zetet@dasd.com>
2021-02-24 01:06:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 Hooks.on("renderDialog", (dialog, $element) => {
-    if (!$element.find(`input[type=text]`)[0] && !$element.find(`input[type=text]`)[0].value) {
+    if ($element.find(`input[type=text]`)[0] && !$element.find(`input[type=text]`)[0].value) {
         $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
         $element.find(`input[type=text]`)[0].focus();
     }
 });
 
 Hooks.on("renderFolderConfig", (dialog, $element) => {
-    if (!!$element.find(`input[type=text]`)[0] && !$element.find(`input[type=text]`)[0].value) {
+    if ($element.find(`input[type=text]`)[0] && !$element.find(`input[type=text]`)[0].value) {
         $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
         $element.find(`input[type=text]`)[0].focus();
     }
 });
 Hooks.on("renderApplication", (dialog, $element) => {
-    if (!!$element.find(`input[type=text]`)[0] && !$element.find(`input[type=text]`)[0].value) {
+    if ($element.find(`div#module-management`) && $element.find(`input[type=text]`)[0]) {
+        $element.find(`input[type=text]`)[0].focus();
+        return;
+    }
+    if ($element.find(`input[type=text]`)[0] && !$element.find(`input[type=text]`)[0].value) {
         $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
         $element.find(`input[type=text]`)[0].focus();
     }
