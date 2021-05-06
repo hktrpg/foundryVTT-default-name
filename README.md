7d606bd7625f0b51ab36d3e7dbbea3c6bfbc6c0b
zeteticl <cougar.l@hotmail.com>
2021-05-06 15:24:39 +0800

    update 1.1.0
    Update option.
    If checked, there will be a be default name by placeholder in box, otherwise will focus only. (Default unchecked)
    
    If you create a new item without input name, it will named 'new'.

--- README.md
+++ README.md
@@ -6,11 +6,20 @@
 
 Focus and with default name when create new Actor, item...etc.
 
+
 ![](./readme.gif)
 
 
 
 ## Changelog
+
+### v1.1.0
+
+Update option. 
+If checked, there will be a be default name by placeholder in box, otherwise will focus only. (Default unchecked)
+
+If you create a new item without input name, it will named 'new'.
+
 
 ### v1.0.0
 
--- index.js
+++ index.js
@@ -1,23 +1,64 @@
+var isDefaultName = false;
+
+Hooks.once("init", () => {
+    game.settings.register("default_name", "isDefaultName", {
+        name: 'is Default Name',
+        hint: 'If checked, there will be a be default name by placeholder in box, otherwise will focus only.',
+        scope: "world",
+        config: true,
+        default: false,
+        type: Boolean
+    });
+    isDefaultName = game.settings.get("default_name", "isDefaultName");
+});
+
 Hooks.on("renderDialog", (dialog, $element) => {
+    isDefaultName = game.settings.get("default_name", "isDefaultName");
     if ($element.find(`input[type=text]`)[0] && !$element.find(`input[type=text]`)[0].value) {
-        $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
+        if (isDefaultName) $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
         $element.find(`input[type=text]`)[0].focus();
     }
 });
 
+
 Hooks.on("renderFolderConfig", (dialog, $element) => {
+    isDefaultName = game.settings.get("default_name", "isDefaultName");
     if ($element.find(`input[type=text]`)[0] && !$element.find(`input[type=text]`)[0].value) {
-        $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
+        if (isDefaultName) $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
         $element.find(`input[type=text]`)[0].focus();
     }
 });
+
 Hooks.on("renderApplication", (dialog, $element) => {
+    isDefaultName = game.settings.get("default_name", "isDefaultName");
     if ($element.find(`div#module-management`) && $element.find(`input[type=text]`)[0]) {
         $element.find(`input[type=text]`)[0].focus();
         return;
     }
     if ($element.find(`input[type=text]`)[0] && !$element.find(`input[type=text]`)[0].value) {
-        $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
+        if (isDefaultName) $element.find(`input[type=text]`)[0].value = $element.find(`input[type=text]`)[0].placeholder || 'New Item';
         $element.find(`input[type=text]`)[0].focus();
     }
+});
+
+Hooks.on("preCreateScene", (dialog) => {
+    if (!dialog.name) dialog.name = 'New';
+});
+Hooks.on("preCreateActor", (dialog) => {
+    if (!dialog.name) dialog.name = 'New';
+});
+Hooks.on("preCreateItem", (dialog) => {
+    if (!dialog.name) dialog.name = 'New';
+});
+Hooks.on("preCreateJournalEntry", (dialog) => {
+    if (!dialog.name) dialog.name = 'New';
+});
+Hooks.on("preCreateRollTable", (dialog) => {
+    if (!dialog.name) dialog.name = 'New';
+});
+Hooks.on("preCreateFolder", (dialog) => {
+    if (!dialog.name) dialog.name = 'New';
+});
+Hooks.on("preCreatePlaylist", (dialog) => {
+    if (!dialog.name) dialog.name = 'New';
 });
